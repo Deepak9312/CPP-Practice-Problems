4d25729fd23258543ed5d99491306e79cfd23b8e
Deepak Chauhan <deepakch9312@gmail.com>
2022-08-09 12:12:09 +0530

    Create Add two numbers.cpp

---
+++ Add two numbers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int num1, num2;
+	cout << "Enter a number: ";
+	cin >> num1;
+	cout << "Enter another number: ";
+	cin >> num2;
+	int sum = num1 + num2;
+	cout << "Sum of two entered number is " << sum;
+	return 0;
+}

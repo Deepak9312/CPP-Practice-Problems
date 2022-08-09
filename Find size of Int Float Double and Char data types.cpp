0adb30c5b73e89ec901db446c91048999d7849e1
Deepak Chauhan <deepakch9312@gmail.com>
2022-08-09 12:29:43 +0530

    Create Find size of Int Float Double and Char data types.cpp

---
+++ Find size of Int Float Double and Char data types.cpp
@@ -0,0 +1,11 @@
+//CPP Program to Find size of Int Float Double and Char data types
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Size of int is " << sizeof(int) << " bytes\n";
+	cout << "Size of float is " << sizeof(float) << " bytes\n";
+	cout << "Size of double is " << sizeof(double) << " bytes\n";
+	cout << "Size of char is " << sizeof(char) << " bytes\n";
+	return 0;
+}

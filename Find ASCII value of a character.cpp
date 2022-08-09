7cb54e8ec94fdcd52c9a72487f049a303f494969
Deepak Chauhan <deepakch9312@gmail.com>
2022-08-09 12:38:40 +0530

    Create Find ASCII value of a character.cpp

---
+++ Find ASCII value of a character.cpp
@@ -0,0 +1,12 @@
+//C++ Program to Find ASCII value of a character
+#include <iostream>
+using namespace std;
+
+int main() {
+	char ch;
+	cout << "Enter a character: ";
+	cin >> ch;
+
+	cout << "ASCII value of the entered character is " << int(ch);
+	return 0;
+}

d3d40810a6e04219232ca922a89ca45885a3ca1f
Deepak Chauhan <deepakch9312@gmail.com>
2022-08-09 12:33:58 +0530

    Create Multiply two numbers.cpp

---
+++ Multiply two numbers.cpp
@@ -0,0 +1,16 @@
+//C++ Program to Multiply two numbers
+#include <iostream>
+using namespace std;
+
+int main() {
+	int num1, num2, product;
+	cout << "Enter first number: ";
+	cin >> num1;
+	
+	cout << "Enter second number: ";
+	cin >> num2;
+
+	product = num1 * num2;
+	cout << "Product of two entered number is " << product;
+	return 0;
+}
